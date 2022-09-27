b80a61f69cb31d20e60d44b7d4f581a366747b93
caijinglong-b-imac <cjl_spy@163.com>
2022-09-27 09:39:36 +0800

    针对 api 33 更新了权限问题

--- android/src/main/kotlin/top/kikt/bt/spp/bluetooth_spp/BluetoothSppPlugin.kt
+++ android/src/main/kotlin/top/kikt/bt/spp/bluetooth_spp/BluetoothSppPlugin.kt
@@ -17,111 +17,133 @@ import top.kikt.bt.spp.bluetooth_spp.core.BluetoothBroadcastReceiver
 import top.kikt.bt.spp.bluetooth_spp.core.BluetoothDeviceConnection
 import top.kikt.bt.spp.bluetooth_spp.core.BluetoothSpp
 
-class BluetoothSppPlugin(private val registrar: Registrar, channel: MethodChannel) : MethodCallHandler {
-  
-  private val sppPlugin: BluetoothSpp = BluetoothSpp(registrar)
-  private val permissions = RxPermissions(registrar.activity() as FragmentActivity)
-  
-  private val bluetoothBroadcastReceiver = BluetoothBroadcastReceiver(registrar, channel)
-  
-  init {
-    registrar.activity().registerReceiver(bluetoothBroadcastReceiver, IntentFilter().apply {
-      addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)
-      addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED)
-      addAction(BluetoothDevice.ACTION_FOUND)
-      addAction(BluetoothDevice.ACTION_NAME_CHANGED)
-      addAction(BluetoothAdapter.ACTION_STATE_CHANGED)
-    })
-  }
-  
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "top.kikt/bluetooth_spp")
-      channel.setMethodCallHandler(BluetoothSppPlugin(registrar, channel))
+class BluetoothSppPlugin(private val registrar: Registrar, channel: MethodChannel) :
+    MethodCallHandler {
+
+    private val sppPlugin: BluetoothSpp = BluetoothSpp(registrar)
+    private val permissions = RxPermissions(registrar.activity() as FragmentActivity)
+
+    private val bluetoothBroadcastReceiver = BluetoothBroadcastReceiver(registrar, channel)
+
+    init {
+        registrar.activity().registerReceiver(bluetoothBroadcastReceiver, IntentFilter().apply {
+            addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)
+            addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED)
+            addAction(BluetoothDevice.ACTION_FOUND)
+            addAction(BluetoothDevice.ACTION_NAME_CHANGED)
+            addAction(BluetoothAdapter.ACTION_STATE_CHANGED)
+        })
     }
-  }
-  
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    val replyHandler = ReplyHandler(call, result)
-    when (call.method) {
-      "enable" -> {
-        checkPermission(replyHandler) {
-          sppPlugin.enable()
+
+    companion object {
+        @JvmStatic
+        fun registerWith(registrar: Registrar) {
+            val channel = MethodChannel(registrar.messenger(), "top.kikt/bluetooth_spp")
+            channel.setMethodCallHandler(BluetoothSppPlugin(registrar, channel))
         }
-      }
-      "supportSpp" -> {
-        val supportSpp = sppPlugin.supportSpp(registrar.context())
-        replyHandler.success(supportSpp)
-      }
-      "disable" -> {
-        checkPermission(replyHandler) {
-          sppPlugin.disable()
-        }
-      }
-      "scan" -> {
-        checkPermission(replyHandler) {
-          sppPlugin.startScan()
-        }
-      }
-      "stop" -> {
-        checkPermission(replyHandler) {
-          sppPlugin.stopScan()
-        }
-      }
-      "getBondDevices" -> {
-        checkPermission(replyHandler) {
-          replyHandler.success(sppPlugin.getBondDevicesList())
-        }
-      }
-      "isEnabled" -> {
-        checkPermission(replyHandler) {
-          replyHandler.success(sppPlugin.isEnabled())
-        }
-      }
-      "conn" -> {
-        checkPermission(replyHandler) {
-          val mac = call.argument<String>("mac")!!
-          
-          val connect = BluetoothDeviceConnection.findConnect(mac)
-          if (connect != null) {
-            replyHandler.success(connect.index)
-            return@checkPermission
-          }
-          
-          val safe = call.argument<Boolean>("safe") ?: false
-          val device = bluetoothBroadcastReceiver.findDevice(mac)
-          if (device == null) {
-            replyHandler.error("没找到蓝牙设备")
-            return@checkPermission
-          }
-          val connection = BluetoothDeviceConnection.buildBluetoothDeviceConnection(registrar, device, safe)
-          replyHandler.success(connection.index)
-        }
-      }
-      else -> replyHandler.notImplemented()
     }
-  }
-  
-  @SuppressLint("CheckResult")
-  private fun checkPermission(handler: ReplyHandler, runnable: () -> Unit) {
-    val info = arrayListOf(
-      Manifest.permission.BLUETOOTH,
-      Manifest.permission.BLUETOOTH_ADMIN,
-      Manifest.permission.ACCESS_FINE_LOCATION)
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-      info.add(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
-    }
-    
-    permissions.request(*info.toTypedArray())
-    
-    permissions.request(*info.toTypedArray())
-      .subscribe {
-        if (it) {
-          runnable()
-        } else {
-          handler.success(-1)
+
+    override fun onMethodCall(call: MethodCall, result: Result) {
+        val replyHandler = ReplyHandler(call, result)
+        when (call.method) {
+            "enable" -> {
+                checkPermission(replyHandler) {
+                    sppPlugin.enable()
+                }
+            }
+            "supportSpp" -> {
+                val supportSpp = sppPlugin.supportSpp(registrar.context())
+                replyHandler.success(supportSpp)
+            }
+            "disable" -> {
+                checkPermission(replyHandler) {
+                    sppPlugin.disable()
+                }
+            }
+            "scan" -> {
+                checkPermission(replyHandler) {
+                    sppPlugin.startScan()
+                }
+            }
+            "stop" -> {
+                checkPermission(replyHandler) {
+                    sppPlugin.stopScan()
+                }
+            }
+            "getBondDevices" -> {
+                checkPermission(replyHandler) {
+                    replyHandler.success(sppPlugin.getBondDevicesList())
+                }
+            }
+            "isEnabled" -> {
+                checkPermission(replyHandler) {
+                    replyHandler.success(sppPlugin.isEnabled())
+                }
+            }
+            "conn" -> {
+                checkPermission(replyHandler) {
+                    val mac = call.argument<String>("mac")!!
+
+                    val connect = BluetoothDeviceConnection.findConnect(mac)
+                    if (connect != null) {
+                        replyHandler.success(connect.index)
+                        return@checkPermission
+                    }
+
+                    val safe = call.argument<Boolean>("safe") ?: false
+                    val device = bluetoothBroadcastReceiver.findDevice(mac)
+                    if (device == null) {
+                        replyHandler.error("没找到蓝牙设备")
+                        return@checkPermission
+                    }
+                    val connection = BluetoothDeviceConnection.buildBluetoothDeviceConnection(
+                        registrar,
+                        device,
+                        safe
+                    )
+                    replyHandler.success(connection.index)
+                }
+            }
+            else -> replyHandler.notImplemented()
         }
-      }
-  }
+    }
+
+    @SuppressLint("CheckResult")
+    private fun checkPermission(handler: ReplyHandler, runnable: () -> Unit) {
+        val info = arrayListOf(
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_ADMIN,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_LOCATION_EXTRA_COMMANDS,
+        )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            info.addAll(
+                listOf(
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+                )
+            )
+        }
+
+        if (Build.VERSION.SDK_INT >= 31) {
+            info.addAll(
+                listOf(
+                    Manifest.permission.BLUETOOTH_ADVERTISE,
+                    Manifest.permission.BLUETOOTH_SCAN,
+                    Manifest.permission.BLUETOOTH_CONNECT,
+                )
+            )
+        }
+
+        permissions.request(*info.toTypedArray())
+
+        permissions.request(*info.toTypedArray())
+            .subscribe {
+                if (it) {
+                    runnable()
+                } else {
+                    handler.success(-1)
+                }
+            }
+    }
 }
