8ed241bf9b9bf7c5e6c21c0b71b53f15e19891ca
caijinglong-b-imac <cjl_spy@163.com>
2022-09-27 11:41:37 +0800

    移除RxPermission 使用原生的方式请求权限

--- android/src/main/kotlin/top/kikt/bt/spp/bluetooth_spp/BluetoothSppPlugin.kt
+++ android/src/main/kotlin/top/kikt/bt/spp/bluetooth_spp/BluetoothSppPlugin.kt
@@ -5,23 +5,24 @@ import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.os.Build
-import androidx.fragment.app.FragmentActivity
-import com.tbruyelle.rxpermissions3.RxPermissions
+import android.util.Log
+import androidx.core.app.ActivityCompat
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry
 import io.flutter.plugin.common.PluginRegistry.Registrar
 import top.kikt.bt.spp.bluetooth_spp.core.BluetoothBroadcastReceiver
 import top.kikt.bt.spp.bluetooth_spp.core.BluetoothDeviceConnection
 import top.kikt.bt.spp.bluetooth_spp.core.BluetoothSpp
 
 class BluetoothSppPlugin(private val registrar: Registrar, channel: MethodChannel) :
-    MethodCallHandler {
+    MethodCallHandler, PluginRegistry.RequestPermissionsResultListener {
 
     private val sppPlugin: BluetoothSpp = BluetoothSpp(registrar)
-    private val permissions = RxPermissions(registrar.activity() as FragmentActivity)
 
     private val bluetoothBroadcastReceiver = BluetoothBroadcastReceiver(registrar, channel)
 
@@ -33,6 +34,8 @@ class BluetoothSppPlugin(private val registrar: Registrar, channel: MethodChanne
             addAction(BluetoothDevice.ACTION_NAME_CHANGED)
             addAction(BluetoothAdapter.ACTION_STATE_CHANGED)
         })
+
+        registrar.addRequestPermissionsResultListener(this)
     }
 
     companion object {
@@ -108,22 +111,20 @@ class BluetoothSppPlugin(private val registrar: Registrar, channel: MethodChanne
         }
     }
 
+    private var nextRunnable: (() -> Unit)? = null
+
+    private var replyHandler: ReplyHandler? = null
+    private var permissionRequestCode = 0x6969
+
     @SuppressLint("CheckResult")
     private fun checkPermission(handler: ReplyHandler, runnable: () -> Unit) {
+
         val info = arrayListOf(
             Manifest.permission.BLUETOOTH,
             Manifest.permission.BLUETOOTH_ADMIN,
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_LOCATION_EXTRA_COMMANDS,
         )
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            info.addAll(
-                listOf(
-                    Manifest.permission.ACCESS_BACKGROUND_LOCATION,
-                )
-            )
-        }
 
         if (Build.VERSION.SDK_INT >= 31) {
             info.addAll(
@@ -135,13 +136,44 @@ class BluetoothSppPlugin(private val registrar: Registrar, channel: MethodChanne
             )
         }
 
-        permissions.request(*info.toTypedArray())
-            .subscribe {
-                if (it) {
-                    runnable()
-                } else {
-                    handler.success(-1)
-                }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            nextRunnable = runnable
+            replyHandler = handler
+
+            ActivityCompat.requestPermissions(registrar.activity(),info.toTypedArray(), permissionRequestCode)
+        } else {
+            runnable()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ): Boolean {
+        if (requestCode != permissionRequestCode) {
+            return false
+        }
+        val notGrandPermissions = ArrayList<String>()
+        grantResults.forEachIndexed { index, it ->
+            if (it != PackageManager.PERMISSION_GRANTED) {
+                notGrandPermissions.add(permissions[index])
             }
+        }
+
+        if (notGrandPermissions.isNotEmpty()) {
+            Log.i("发生权限问题", "未给与 ${notGrandPermissions.toList()} 权限")
+            nextRunnable?.invoke()
+
+            nextRunnable = null
+            replyHandler = null
+            return true
+        }
+
+        nextRunnable?.invoke()
+        nextRunnable = null
+        replyHandler = null
+
+        return true
     }
 }
